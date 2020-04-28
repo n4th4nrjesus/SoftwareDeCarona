c1eaa2fa9532eb7a4a1552cdfd1c99140f9f5104
hrmshnrq <55238606+hrmshnrq@users.noreply.github.com>
2020-04-28 18:10:09 -0300

    adicinando form para oferecer carona

--- discContratar.php
+++ discContratar.php
@@ -48,21 +48,39 @@ Software de Carona
                 ?>
 
                 <div class="w3-responsive w3-card-4">
-                    <div class="w3-container w3-theme">
-                        <h2>Informe os dados da nova Disciplina</h2>
+                    <div class="w3-container w3-teal">
+                        
+                        <h2>Informe os dados da sua carona</h2>
                     </div>
-                    <form class="w3-container" action="DiscContratar_exe.php" method="post" onsubmit="return check(this.form)">
+                    <form class="w3-container" action="ProfContratar_exe.php" method="post" onsubmit="return check(this.form)">
 						<input type="hidden" id="acaoForm" name="acaoForm" value="Contratar">
 						<p>
-						<label class="w3-text-deep-purple"><b>Nome</b></label>
-						<input class="w3-input w3-border w3-light-grey" name="Nome" type="text" pattern="[a-zA-Z\u00C0-\u00FF ]{10,100}$"
-							   title="Nome da disciplina entre 10 e 100 letras." required></p>
+						<label class="w3-text-teal"></label></p><h6><b>Insira o local de partida</b></h6>
+						<input class="w3-input w3-border w3-light-grey" name="Nome" type="text" pattern="[a-zA-Z\u00C0-\u00FF ]{10,100}$" title="Endereço entre 10 e 100 letras." required=""><p></p>
 						<p>
-						<label class="w3-text-deep-purple"><b>Ementa</b></label>
-						<textarea class="w3-input w3-border w3-light-grey " name="Ementa"  rows="5" title="Texto Descritivo" required></textarea>
+						<label class="w3-text-teal"></label></p><h6><b>Insira o endereço de destino</b></h6>
+						<input class="w3-input w3-border w3-light-grey " name="Celular" type="text" pattern="\([0-9]{2}\)[0-9]{4,6}-[0-9]{3,4}$" title="(11)1111-1111." required=""><p></p>
 						<p>
-						<input type="submit" value="Registrar" class="w3-btn w3-theme" >
-						<input type="button" value="Cancelar" class="w3-btn w3-theme" onclick="window.location.href='.'"></p>
+						<label class="w3-text-teal"></label></p><h6><b>Selecione o gênero de preferência do motorista</b></h6>
+
+                        <select class="w3-input w3-border w3-light-grey" name="DataNasc">
+                            <option value="qualquer">Qualquer</option>
+                            <option value="masculino">Masculino</option>
+                            <option value="feminino">Feminino</option>
+                        </select>
+						<p>
+						<label class="w3-text-teal"></label></p><h6>Login</h6>
+						<input class="w3-input w3-border w3-light-grey" name="Login" type="text" pattern="[a-zA-Z]{2,20}.[a-zA-Z]{2,20}" title="Formato: nome.sobrenome" required=""><p></p>
+						<p>
+						<label class="w3-text-teal"></label></p><h6>Senha</h6>
+						<input class="w3-input w3-border w3-light-grey" name="Senha" type="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,8}" title="Deve conter ao menos um número, uma letra maiúscula e minúscula, um caracter especial, com 6 a 8 caracteres" required=""><p></p>
+						<p>
+						<label class="w3-text-teal"></label></p><h6>Confirma Senha</h6>
+						<input class="w3-input w3-border w3-light-grey" name="Senha2" type="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,8}" title="Deve conter ao menos um número, uma letra maiúscula e minúscula, um caracter especial, com 6 a 8 caracteres" required=""> <p></p> 
+						
+						<p>
+						<input type="submit" value="Registrar" class="w3-btn w3-teal" >
+						<input type="button" value="Cancelar" class="w3-btn w3-teal" onclick="window.location.href='.'"></p>
 					</form>
 				</div>
 
@@ -74,7 +92,7 @@ Software de Carona
 	<footer class="w3-panel w3-padding-32 w3-card-4 w3-light-grey w3-center w3-opacity">
     <p>
         <nav>
-            <a class="w3-button w3-theme w3-hover-white"
+            <a class="w3-button w3-teal w3-hover-white"
                onclick="document.getElementById('id01').style.display='block'">Sobre</a>
         </nav>
     </p>
